5e061e48c36fa61bd5c6d8a5a2e73af721484250
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-07-07 15:14:02 -0700

    Update harddrive_info.sh

--- Scripts/harddrive_info.sh
+++ Scripts/harddrive_info.sh
@@ -1,1 +1,3 @@
+# Quick script that will pull the information from the drives in your system.
+#!/bin/bash
 for disk in $(lsblk -nd -o NAME); do echo "/dev/$disk: $(udevadm info --query=all --name=/dev/$disk | grep ID_SERIAL"; done
