e97d3474c2e1c5696fd3a1aa14f7704fa3319b2d
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-07-07 14:08:26 -0700

    Create RHEL_7.9_Bootloader_change.sh

---
+++ Scripts/RHEL_7.9_Bootloader_change.sh
@@ -0,0 +1,9 @@
+#run as root and reboot to confirm changes. Then re-run the first grubby command.
+
+#!/bin/bash
+set -e
+#Check current configuration
+grubby --info=$(grubby --default-kernel)
+sleep 2
+#Modify CPUs to isolate the amount of cores requested 
+grubby --update-kernel=$(grubby --default-kernel) --args="isolcpus=1-29"
