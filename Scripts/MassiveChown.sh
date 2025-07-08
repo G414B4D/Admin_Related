b91a63b3e4c9bc8f27c7c2f2fb7d239bacdc4d61
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-07-08 11:01:12 -0700

    Update MassiveChown.sh

--- Scripts/MassiveChown.sh
+++ Scripts/MassiveChown.sh
@@ -1,5 +1,6 @@
+#! /bin/bash
 #Script will pass a created list of folders/directories (one per line), and set ownership of all to root:root
-#!/bin/bash
+
 read -p "Enter the full path to your list of folders/directories to chown: " LIST
 echo""
 cat $LIST | xargs -I {} chown root:root "{}"
