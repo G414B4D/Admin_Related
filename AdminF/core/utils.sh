e241063b397ae07ce1d0d889caf0d794956f9ef9
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-08-06 07:07:16 -0700

    Update utils.sh

--- AdminF/core/utils.sh
+++ AdminF/core/utils.sh
@@ -1,1 +1,18 @@
-# Will house shared helper functions
+##
+#!/bin/bash
+
+run_module() {
+  local module_path="modules/$1"
+  if [[ ! -x "$module_path" ]]; then
+    echo "Module not found or not executable: $module_path"
+    return 1
+  fi
+
+  log "Running module: $1"
+  bash "$module_path"
+}
+
+log() {
+  local logfile="log/$(date +%F).log"
+  echo "[$(date '+%T')] $*" | tee -a "$logfile"
+}
