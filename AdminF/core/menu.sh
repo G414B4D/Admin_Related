0a5f48a18053b39b5fb10e88b41bf6668723850d
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-08-06 06:59:52 -0700

    Update menu.sh
    Added some contextual options for initial testing of plan. More options to be added at a later date.

--- AdminF/core/menu.sh
+++ AdminF/core/menu.sh
@@ -1,1 +1,18 @@
-# Text menu for AdminF as a whole
+#/bin/bash
+
+show_menu() {
+  echo "=== Admin Framework For General Tasks === "
+  echo "1) Create User"
+  echo "2) Show IP Info"
+  echo "3) Update Packages"
+  echo "4) exit"
+  read -rp "Please select an option: " choice
+
+  case "$choice" in
+    1) run_modules users/create_user.sh ;;
+    2) run_modules network/show_ip.sh ;;
+    3) run_modules packages/update_all.sh ;;
+    4) exit 0 ;;
+    *) echo "Invalid choice"; sleep 1; show_menu ;;
+  esac
+}    
