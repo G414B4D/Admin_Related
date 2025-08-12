0571d63b66a29c1ef46a2094d10183126d647e14
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-08-12 15:54:25 -0700

    Update menu.sh
    added root_copy option and corrected typo

--- AdminF/core/menu.sh
+++ AdminF/core/menu.sh
@@ -1,12 +1,13 @@
 #/bin/bash
 
 show_menu() {
-  echo "=== Admin Framework For General Tasks === "
+  echo "=== Admin Framework For General Tasks: AdminF === "
   echo "1) Create User"
   echo "2) Show IP Info"
   echo "3) Update Packages"
-  echo "5) Update root password age to now"
-  echo "4) exit"
+  echo "4) Update root password age to now"
+  echo "5) Rsync file from remote host"
+  echo "6) exit"
   read -rp "Please select an option: " choice
 
   case "$choice" in
@@ -14,7 +15,8 @@ show_menu() {
     2) run_modules network/show_ip.sh ;;
     3) run_modules packages/update_all.sh ;;
     4) run_modules accounts/root_PW_date.sh ;;
-    5) exit 0 ;;
+    5) run_modules root_copy.sh ;;
+    6) exit 0 ;;
     *) echo "Invalid choice"; sleep 1; show_menu ;;
   esac
 }    
