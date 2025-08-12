74089826bb4ea2e128e24245e4fb7967bc5f3da4
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-08-12 15:32:41 -0700

    Update menu.sh
    Added pw age module

--- AdminF/core/menu.sh
+++ AdminF/core/menu.sh
@@ -5,6 +5,7 @@ show_menu() {
   echo "1) Create User"
   echo "2) Show IP Info"
   echo "3) Update Packages"
+  echo "5) Update root password age to now"
   echo "4) exit"
   read -rp "Please select an option: " choice
 
@@ -12,7 +13,8 @@ show_menu() {
     1) run_modules users/create_user.sh ;;
     2) run_modules network/show_ip.sh ;;
     3) run_modules packages/update_all.sh ;;
-    4) exit 0 ;;
+    4) run_modules accounts/root_PW_date.sh ;;
+    5) exit 0 ;;
     *) echo "Invalid choice"; sleep 1; show_menu ;;
   esac
 }    
